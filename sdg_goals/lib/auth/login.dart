f178b34a471bc276f73ead7a6945ee6c1e7af9a3
BrightonMboya <b.mboya@alustudent.com>
2023-02-13 20:45:56 +0200

    This commit should be ignored

--- sdg_goals/lib/auth/login.dart
+++ sdg_goals/lib/auth/login.dart
@@ -1,11 +1,43 @@
-import 'package:flutter/src/widgets/container.dart';
-import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/material.dart';
 
 class Login extends StatelessWidget {
   const Login({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return 
+    // return GridView.count(
+    //   crossAxisCount: 2,
+    //   padding: const EdgeInsets.all(20),
+    //   crossAxisSpacing: 10,
+    //   mainAxisSpacing: 10,
+    //   children: <Widget>[
+    //     Container(
+    //       padding: const EdgeInsets.all(5),
+    //       color: Colors.amber[200],
+    //       child: const Text("Hello World"),
+    //     ),
+    //     Container(
+    //       padding: const EdgeInsets.all(5),
+    //       color: Colors.amber[200],
+    //       child: const Text("Hello World"),
+    //     ),
+    //     Container(
+    //       padding: const EdgeInsets.all(5),
+    //       color: Colors.amber[200],
+    //       child: const Text("Hello World"),
+    //     ),
+    //     Container(
+    //       padding: const EdgeInsets.all(5),
+    //       color: Colors.amber[200],
+    //       child: const Text("Hello World"),
+    //     ),
+    //     Container(
+    //       padding: const EdgeInsets.all(5),
+    //       color: Colors.amber[200],
+    //       child: const Text("Hello World"),
+    //     )
+    //   ],
+    // );
   }
 }
